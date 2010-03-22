0cc73ae0ddf56c14db3ef23d9c2a85bf5b565c63
anatoly techtonik <techtonik@gmail.com>
2010-03-23 00:37:44 +0200

    because setup.py code executes on user machine, split copy and svn revision extraction part into refresh.py script that should be run prior to setup.py bdist

---
+++ refresh.py
@@ -0,0 +1,41 @@
+"""
+This script copies latest upload.py to review.py and refreshes
+version in setup.py to upload `review` package to PyPI.
+"""
+
+import shutil
+
+
+def get_svn_path_revision(path):
+    """return latest modification revision of a path inside svn
+
+       :raise:  EnvironmentError if SVN working copy format is unknown
+       :return: int
+    """
+    from os.path import basename, dirname, join, isdir
+
+    if isdir(path):
+        entries_path = join(path, ".svn", "entries")
+    else:
+        entries_path = join(dirname(path), ".svn", "entries")
+    with open(entries_path) as f:
+        entries = [l.strip() for l in f.readlines()]
+
+    # check known working copy version
+    # it seems to be entries[0]
+    if entries[0] != '10':
+        raise EnvironmentError(10, "Unknown SVN working copy format")
+
+    # check entry modification revision
+    if isdir(path):
+        return int(entries[10])
+    else:
+        idx = entries.index(basename(path))
+        entry = entries[idx:idx+33]
+        return int(entry[9])
+
+# copy upload.py to review.py
+upload_py_path = "../../static/upload.py"
+shutil.copyfile(upload_py_path, "review.py")
+# replace version/revision in setup.py
+version = get_svn_path_revision(upload_py_path)
--- setup.py
+++ setup.py
@@ -1,67 +1,31 @@
 """
-Package `upload.py` script as `review.py` for uploading to PyPI
-as executable module that can be then called as::
+This setup.py packages `upload.py` script from rietveld project
+as `review.py` for PyPI, so that it could be called as::
 
   python -m review
 
+To update PyPI package, issue::
+   python refresh.py
+   python setup.py sdist upload
 """
 
-# To update PyPI package, issue
-#   python setup.py sdist upload
-
-
 from distutils.core import setup
-import shutil, sys
 
+setup(
+    name='review',
+    version='r100',
+    author='Google Inc. & Co.',
 
-def get_svn_path_revision(path):
-    """return latest modification revision of a path inside svn
-
-       :raise:  EnvironmentError if SVN working copy format is unknown
-       :return: int
-    """
-    from os.path import basename, dirname, join, isdir
-
-    if isdir(path):
-        entries_path = join(path, ".svn", "entries")
-    else:
-        entries_path = join(dirname(path), ".svn", "entries")
-    with open(entries_path) as f:
-        entries = [l.strip() for l in f.readlines()]
-
-    # check known working copy version
-    # it seems to be entries[0]
-    if entries[0] != '10':
-        raise EnvironmentError(10, "Unknown SVN working copy format")
-
-    # check entry modification revision
-    if isdir(path):
-        return int(entries[10])
-    else:
-        idx = entries.index(basename(path))
-        entry = entries[idx:idx+33]
-        return int(entry[9])
-
-upload_py_path = "../../static/upload.py"
-version = get_svn_path_revision(upload_py_path)
-shutil.copyfile(upload_py_path, "review.py")
-description = """
+    description='rietveld code review upload script',
+    long_description=description = """
 Usage::
 
     python -m review [options]
 
 * Home Page:   http://codereview.appspot.com/
 * Development: http://code.google.com/p/rietveld/
-* Discussions: http://groups.google.com/group/codereview-discuss
-"""
-
-setup(
-    name='review',
-    version='r%s' % version,
-    author='Google Inc. & Co.',
-
-    description='rietveld code review upload script',
-    long_description=description,
+* Discussions: http://groups.google.com/group/codereview-discuss/
+""",
     license='Apache 2.0',
     classifiers=[
         'Environment :: Console',
