6f6951220666511b4368accd723c92bfc9c05831
anatoly techtonik <techtonik@gmail.com>
2011-10-21 18:19:55 +0200

    release 709 with repository IDs - Yay!

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from distutils.core import setup
 
 setup(
     name='review',
-    version='695',
+    version='709',
     author='Google Inc. & Co.',
     author_email='codereview-discuss@googlegroups.com',
     url='http://codereview.appspot.com/',
@@ -27,10 +27,15 @@ Usage::
 
 Changes:
 
-On August 1st of 2011 Rietveld moved from Subversion to Mercurial.
-As a result of ommitting some branches, revision number decreased.
+ On August 1st of 2011 Rietveld moved from Subversion to Mercurial.
+ As a result of ommitting some branches, revision number decreased.
 
-`695:ba3f47e4a614`_ - 2011-09-07
+ `709:840f9bb917ba`_ - 2011-09-22
+
+ - Add repository ID field to link Issue with Repository (review 5093045)
+ - More fixes to fit upload.py options in one screen (review 4962070)
+
+ `695:ba3f47e4a614`_ - 2011-09-07
 
  - Hide perforce options unless explicitly requested (4968071)
  - --send_patch option to attach diff instead of inline, by Kaelyn (4881041)
@@ -68,6 +73,7 @@ Before moving to Mercurial:
  | r534 - Convert Unicode values to ASCII (1590044)
  | r531 - Add account type flag to upload.py (issue #195)
 
+.. _`709:840f9bb917ba`: http://code.google.com/p/rietveld/source/list?path=/upload.py&r=840f9bb917ba
 .. _`695:ba3f47e4a614`: http://code.google.com/p/rietveld/source/list?path=/upload.py&r=ba3f47e4a614
 
 * Home Page:   http://codereview.appspot.com/
